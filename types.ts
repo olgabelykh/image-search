d6af8f5c42ac99b2395ae47b2b45a0bc27b3bf01
Olga Belykh <olgabelykh@Olgas-MacBook-Pro.local>
2023-12-18 09:28:42 +0900

    add types

---
+++ types.ts
@@ -0,0 +1,23 @@
+interface ImageData {
+  id: string;
+  src: string;
+  width: number;
+  height: number;
+  alt: string;
+}
+
+interface ImagesData {
+  results: ImageData[];
+  total: number;
+  total_pages: number;
+}
+
+interface ErrorData {
+  error: string;
+}
+
+type SearchParams = {
+  [key: string]: string;
+};
+
+export type { ErrorData, ImageData, ImagesData, SearchParams };
